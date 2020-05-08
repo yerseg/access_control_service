8158e6630e161dedebebd5d2d302231c7b2d8ca0
Anton <savchenko_a@icloud.com>
2020-05-08 20:19:27 +0300

    add test scenario and base test

---
+++ test_control_service.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+CONF=./conf
+FILE_FOR_TEST=test_file.txt
+RIGHT_PERMISSIONS=644
+
+touch FILE_FOR_TEST
+
+echo "$FILE_FOR_TEST:$RIGHT_PERMISSIONS:root:root" >> $CONF
+
+function compareParameters {
+	echo $1
+	if [[ $2 == $3 ]]
+	then 
+		echo "Test OK"
+	else 
+		echo "Test Error"
+		echo "Got $2, when expect $3"
+}
+
+# first test
+chmod 744 $FILE_FOR_TEST 
+./control_service.sh &
+CONTROL_SERVICE_PID=$!
+sleep 2
+changedPermissions=$(stat -c %a $FILE_FOR_TEST)
+compareParameters "first test" $changedPermissions $RIGHT_PERMISSIONS
+
+rm $FILE_FOR_TEST
+
+# delete last line from CONF
+# containing info about FILE_FOR_TEST
+sed -i '$ d' $CONF 
