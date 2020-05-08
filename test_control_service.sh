df57370c78ab56dd6b0f54610acf0be83a20d28d
Anton <savchenko_a@icloud.com>
2020-05-08 21:21:39 +0300

    add another 2 tests - sigusr1 and delay

--- test_control_service.sh
+++ test_control_service.sh
@@ -3,29 +3,42 @@
 CONF=./conf
 FILE_FOR_TEST=test_file.txt
 RIGHT_PERMISSIONS=644
+DELAY=60 #sec
 
 touch $FILE_FOR_TEST
 
 echo "$FILE_FOR_TEST:$RIGHT_PERMISSIONS:root:root" >> $CONF
 
 function compareParameters {
-	echo $1
 	if [[ $2 == $3 ]]
 	then 
-		echo "Test OK"
+		echo "[+] $1 - OK"
 	else 
-		echo "Test Error"
+		echo "[-] $1 - Error"
 		echo "Got $2, when expect $3"
 	fi
 }
 
-# first test
-chmod 744 $FILE_FOR_TEST 
+chmod 777 $FILE_FOR_TEST 
 ./control_service.sh &
 CONTROL_SERVICE_PID=$!
 sleep 2
 changedPermissions=$(stat -c %a $FILE_FOR_TEST)
-compareParameters "first test" $changedPermissions $RIGHT_PERMISSIONS
+compareParameters "Basic test" $changedPermissions $RIGHT_PERMISSIONS
+
+chmod 777 $FILE_FOR_TEST 
+kill -USR1 $CONTROL_SERVICE_PID
+sleep 2
+changedPermissions=$(stat -c %a $FILE_FOR_TEST)
+compareParameters "SIGUSR1 test" $changedPermissions $RIGHT_PERMISSIONS
+
+chmod 777 $FILE_FOR_TEST
+echo "Need to wait $DELAY sec"
+sleep $DELAY
+changedPermissions=$(stat -c %a $FILE_FOR_TEST)
+compareParameters "Periodic work test" $changedPermissions $RIGHT_PERMISSIONS
+
+echo "If you see OK for 3 times then all work well"
 
 rm $FILE_FOR_TEST
 
