0046d64e8bee48869797ce4dd56800d5a8e818c0
Sergey Kazmin <43613813+yerseg@users.noreply.github.com>
2020-06-01 20:10:10 +0300

    New_dev (#20)
    * update README
    
    * add few test-cases
    
    * remove old SELINUX folder
    
    * add new selinux src with policy
    
    * update package configs in .spec
    
    * remove bad folder
    
    * update readme for rpm
    
    * remove bad flag
    
    * remove useless man file
    
    * remove useless directory

--- test_access_control.sh
+++ test_access_control.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 CONF=/opt/access_control/access_settings.conf
-FILE_FOR_TEST=~/test_file.txt
+FILE_FOR_TEST=/etc/filesystems
 RIGHT_PERMISSIONS=644
+RIGHT_OWNER=root
+RIGHT_GROUP=root
 DELAY=60 #sec
 
 touch $FILE_FOR_TEST
@@ -20,11 +22,20 @@ function compareParameters {
 }
 
 chmod 777 $FILE_FOR_TEST 
-./control_service.sh &
-CONTROL_SERVICE_PID=$!
+CONTROL_SERVICE_PID=$(pgrep access_control)
 sleep 2
 changedPermissions=$(stat -c %a $FILE_FOR_TEST)
-compareParameters "Basic test" $changedPermissions $RIGHT_PERMISSIONS
+compareParameters "Basic test permissions" $changedPermissions $RIGHT_PERMISSIONS
+
+chown $USER $FILE_FOR_TEST 
+sleep 2
+changedOwner=$(stat -c %U $FILE_FOR_TEST)
+compareParameters "Basic test owner" $changedOwner $RIGHT_OWNER
+
+chgrp $USER $FILE_FOR_TEST 
+sleep 2
+changedGrp=$(stat -c %G $FILE_FOR_TEST)
+compareParameters "Basic test group" $changedGrp $RIGHT_GROUP
 
 chmod 777 $FILE_FOR_TEST 
 kill -USR1 $CONTROL_SERVICE_PID
@@ -38,7 +49,7 @@ sleep $DELAY
 changedPermissions=$(stat -c %a $FILE_FOR_TEST)
 compareParameters "Periodic work test" $changedPermissions $RIGHT_PERMISSIONS
 
-echo "If you see OK for 3 times then all work well"
+echo "If you see OK for 5 times then all work well"
 
 rm $FILE_FOR_TEST
 
